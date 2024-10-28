dd551c468dce95ba94c420e1d63b2b8a7cb1f07a
fgnm <fgnm.dev@gmail.com>
2024-10-28 08:03:40 +0100

    empty project to test leaks

--- core/src/main/java/games/rednblack/miniaudiotest/MiniAudioTest.java
+++ core/src/main/java/games/rednblack/miniaudiotest/MiniAudioTest.java
@@ -20,84 +20,49 @@ import games.rednblack.miniaudio.mix.MASplitter;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MiniAudioTest extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture image;
-	private MiniAudio miniAudio;
-	private MASound maSound;
-	private MAGroup maGroup;
-	private MADelayNode delayNode;
-	private IPlatform platform;
-	private float angle;
+
 
 	public MiniAudioTest(IPlatform platform) {
-		this.platform = platform;
+
 	}
 
 	@Override
 	public void create() {
-		batch = new SpriteBatch();
-		image = new Texture("libgdx.png");
-		miniAudio = new MiniAudio();
-		miniAudio.setupAndroid(platform.getAssetsManager());
-		maGroup = miniAudio.createGroup();
-		maGroup.setSpatialization(true);
-		//int res = miniAudio.playSound("game.ogg");
-		//System.out.println(res);
-		maSound = miniAudio.createSound("click.mp3", (short) 0, maGroup);
-		delayNode = new MADelayNode(miniAudio, 0.2f, 0.5f);
-		/*miniAudio.attachToEngineOutput(delayNode, 0);
-		delayNode.attachToThisNode(maGroup, 0);*/
-		MASplitter splitter = new MASplitter(miniAudio);
-		MALowPassFilter lowPassFilter = new MALowPassFilter(miniAudio, 550, 8);
-		MAReverbNode delayNode = new MAReverbNode(miniAudio);
-		miniAudio.attachToEngineOutput(lowPassFilter, 0);
-		miniAudio.attachToEngineOutput(delayNode, 0);
-		lowPassFilter.attachToThisNode(splitter, 0);
-		delayNode.attachToThisNode(splitter, 1);
-		//splitter.attachToThisNode(maGroup, 0);
-		//maSound.loop();
-		System.out.println(maSound.getLength());
+
 	}
 
 	float count;
-
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(image, 140, 210);
-		batch.end();
 		count += Gdx.graphics.getDeltaTime();
 		if(count > 0.2F){
-			long m = Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
-			System.out.println(m / 10000);
-			maSound.play();
+			long m = (Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory()) / 10000;
+			System.out.println(m);
+			if (m > 1000) {
+				System.gc();
+			}
 			count = 0;
 		}
-		//System.out.println(maSound.getCursorPosition());
-		angle += MathUtils.PI / 4f / 100f;
-		//maGroup.setPosition(MathUtils.sin(angle), 0f, -MathUtils.cos(angle));
 	}
 
 	@Override
 	public void pause() {
-		miniAudio.stopEngine();
+		//miniAudio.stopEngine();
 		super.pause();
 	}
 
 	@Override
 	public void resume() {
-		miniAudio.startEngine();
+		//miniAudio.startEngine();
 		super.resume();
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
+		/*batch.dispose();
 		image.dispose();
 		maSound.dispose();
 		delayNode.dispose();
-		miniAudio.dispose();
+		miniAudio.dispose();*/
 	}
 }
